7e7355550e7039ab9fb5ca846e7fc9e159343d47
JustJoshua <justjoshua@gmail.com>
2021-11-09 11:49:40 -0500

    started on chat application with firebase

---
+++ client/frontendchatbox/src/components/comments/Comments.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Comments = (props) => {
+return (
+    <div>
+      Comments
+    </div>
+  )
+};
+
+export default Comments;
---
+++ client/frontendchatbox/src/components/maps/Maps.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Maps = (props) => {
+return (
+    <div>
+      Maps
+    </div>
+  )
+};
+
+export default Maps;
