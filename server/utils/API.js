30f7e61a49a74d973c993a93d37daeb90fd472ce
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-11-08 19:39:05 -0500

    front end queries/mutations

---
+++ server/utils/API.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const BASEURL = process.env.REACT_APP_BASEURL;
+const APIKEY = process.env.REACT_APP_APIKEY;
+
+const search = async (query) =>
+  axios.get(`${BASEURL}${query}${APIKEY});
+
+export default { search };
+
+https://api.yelp.com/v3/businesses/search?term= {{VARIABLE_FOOD_SEARCh_TERM}} &location= {{VARIABLE_USER_LOCATION}}
