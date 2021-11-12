5d253f56cb1562a212e1d52f0b663f19a29b343f
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-11-11 22:53:55 -0500

    deploying

---
+++ client/src/utils/API.js
@@ -0,0 +1,18 @@
+//API LINK
+//https://api.yelp.com/v3/businesses/search?term=&location=
+
+// export default { 
+//   searchYelp : function (food, area) {
+//   return fetch(`https://api.yelp.com/v3/businesses/search?term=${food}&location=${area}`)
+//   }
+// };
+
+const searchYelp = (food, area) => {
+  return fetch(`https://api.yelp.com/v3/businesses/search?term=${food}&location=${area}`)
+  }
+
+
+export default searchYelp;
+
+
+
