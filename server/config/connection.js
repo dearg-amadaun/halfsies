4f9bc8f293e91445bb72d772f953413a1ebf16cb
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-11-12 16:32:35 -0500

    debug connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/halfsies',
+  process.env.MONGODB_URI || 'mongodb+srv://halfsiesmaster:tKOiYzOooQ8sQE4A@sandbox.jadwj.mongodb.net/halfsies?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    // useCreateIndex: true,
-    // useFindAndModify: false,
+    useCreateIndex: true,
+    useFindAndModify: false,
   }
 );
 
 module.exports = mongoose.connection;
+
