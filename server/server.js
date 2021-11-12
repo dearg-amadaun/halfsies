4795b6b3ba4e0b4eaae2e1c9fbda857cf3c46575
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-11-12 13:49:03 -0500

    debug package.json

--- server/server.js
+++ server/server.js
@@ -19,11 +19,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Serve up static assets
-app.use('/images', express.static(path.join(__dirname, '../client/images')));
+// app.use(express.static(path.join(__dirname, '../client/public')));
+app.use(express.static(path.join(__dirname, '../client/public')));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+ });
+ 
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
