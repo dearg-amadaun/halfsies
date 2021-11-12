52de261a0cda5864500b51584dc1249f2335573f
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-11-12 16:53:45 -0500

    debug

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb+srv://halfsiesmaster:tKOiYzOooQ8sQE4A@sandbox.jadwj.mongodb.net/halfsies?retryWrites=true&w=majority',
+  process.env.MONGODB_URI || 'mongodb+srv://halfsiesmaster:jUmMsThKcbASJnOk@sandbox.jadwj.mongodb.net/halfsies?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
